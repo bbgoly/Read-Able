15fefad6e3cdf8dcb7e6eafc7dbf3dafdc184921
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-11-10 13:02:51 -0500

    merge every branch into main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
-
 import TTSComponent from "./components/TTScomponent";
 import FontChangeComponent from "./components/FontChangeComponent";
 import FontSizeComponent from "./components/FontSizeComponent";
 import ReadingModeComponent from "./components/ReadingModeComponent";
 import TaskManager from "./components/TaskManager";
 import DyslexiaAssist from "./components/DyslexiaRuler";
+
+import "./App.css";
 
 function App() {
   return (
